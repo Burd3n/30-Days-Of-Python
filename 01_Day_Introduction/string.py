2acad89cea6baadaf68bc05f887c6de037ddc63a
John Cena <yang_run1996@outlook.com>
2024-10-28 23:41:31 +0800

    added some practice files

---
+++ 01_Day_Introduction/string.py
@@ -0,0 +1,30 @@
+"""
+print("1 + 1")
+
+lst = ["world", "hello"]
+
+tuple = (1, 2, 3)
+
+#str = ['w', 'o', 'r', 'l', 'd']
+
+
+print(help(str))
+
+
+name = True
+"""
+
+first_name = input('What is your name: ')
+age = input('How old are you? ')
+
+print(first_name)
+print(age)
+print(first_name)
+
+first_name = input('What is your name: ')
+age = input('How old are you?')
+
+print(first_name)
+print(age)
+
+print()
